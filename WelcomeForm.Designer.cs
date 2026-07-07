bb7ed754559f5fd26344e6df00723335821017f8
Mohammad Idres <mohammad24x.kl@gmail.com>
2026-07-07 12:31:18 +0300

    Add files via upload

---
+++ WelcomeForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Curse_14_project1Solution
+{
+    partial class WelcomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoToOrderForm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GoToOrderForm
+            // 
+            this.GoToOrderForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.GoToOrderForm.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoToOrderForm.ForeColor = System.Drawing.Color.Moccasin;
+            this.GoToOrderForm.Location = new System.Drawing.Point(183, 399);
+            this.GoToOrderForm.Name = "GoToOrderForm";
+            this.GoToOrderForm.Size = new System.Drawing.Size(111, 57);
+            this.GoToOrderForm.TabIndex = 0;
+            this.GoToOrderForm.Text = "Next";
+            this.GoToOrderForm.UseVisualStyleBackColor = false;
+            this.GoToOrderForm.Click += new System.EventHandler(this.GoToOrderForm_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Moccasin;
+            this.label1.Location = new System.Drawing.Point(40, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 149);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PIZZA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Moccasin;
+            this.label2.Location = new System.Drawing.Point(84, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 59);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CAFFEE";
+            // 
+            // WelcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::Curse_14_project1Solution.Properties.Resources.ai_generated_flat_lay_with_pizza_delicious_pizza_with_vegetables_and_sausage_top_view_with_space_for_text_photo__1__removebg_preview;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1227, 584);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GoToOrderForm);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "WelcomeForm";
+            this.Text = "Pizza Caffee";
+            this.Load += new System.EventHandler(this.WelcomeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GoToOrderForm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WelcomeForm.cs
@@ -0,0 +1,34 @@
+﻿using Pizza;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curse_14_project1Solution
+{
+    public partial class WelcomeForm : Form
+    {
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WelcomeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GoToOrderForm_Click(object sender, EventArgs e)
+        {
+            Form f = new OrderPizzaForm();
+            f.Show();
+        }
+
+       
+    }
+}
